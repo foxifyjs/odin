03f4ec28784890c363a3459fd5127621a81f26fd
Ardalan Amini <ardalan1074@gmail.com>
2018-05-24 01:55:41 +0430

    Added some tests

--- test/environment.js
+++ test/environment.js
@@ -1,8 +1,9 @@
 const NodeEnvironment = require("jest-environment-node");
+const MongoClient = require("mongodb");
 
 class MongoEnvironment extends NodeEnvironment {
   async setup() {
-    this.global.__MONGO_URI__ = await global.__MONGOD__.getConnectionString();
+    this.global.__MONGO_CONNECTION__ = await MongoClient.connect(await global.__MONGOD__.getConnectionString());
     this.global.__MONGO_DB_NAME__ = global.__MONGO_DB_NAME__;
 
     await super.setup();
