9cb96d6670fc0b470c2a92854c7c351124f9b92b
Ardalan Amini <ardalanamini22@gmail.com>
2019-01-03 22:14:32 +0330

    WIP

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,8 @@
+const http = require("http");
 const fs = require("fs");
+const {
+  execSync
+} = require("child_process");
 const rimraf = require("rimraf");
 const path = require("path");
 const portfinder = require("portfinder");
@@ -9,9 +13,43 @@ const {
   Sharded
 } = require("mongodb-topology-manager");
 
+const mongod_path = path.resolve(__dirname, "mongodb");
 const dbpath = path.resolve(__dirname, "db");
 
-const prepare = () => new Promise((resolve, reject) => {
+const download = () => new Promise(async (resolve, reject) => {
+  const dl_version = "mongodb-linux-x86_64-latest.tgz";
+  const dl_path = path.resolve(__dirname, dl_version);
+
+  const file = fs.createWriteStream(dl_path);
+
+  http
+    .get(`http://fastdl.mongodb.org/linux/${dl_version}`, (res) => {
+      res.pipe(file);
+
+      file.on('finish', () => {
+        file.close(() => {
+          execSync(`tar -zxvf ${dl_path} -C ${mongod_path} --strip-components=1`);
+
+          resolve();
+        });
+      });
+    })
+    .on("error", reject);
+});
+
+const prepare = () => new Promise(async (resolve, reject) => {
+  if (!fs.existsSync(mongod_path)) {
+    fs.mkdirSync(mongod_path);
+
+    try {
+      await download();
+    } catch (error) {
+      fs.rmdirSync(mongod_path);
+
+      return reject(error);
+    }
+  }
+
   if (fs.existsSync(dbpath)) {
     rimraf(dbpath, (err) => {
       if (err) return reject(err);
@@ -29,7 +67,7 @@ const DB_NAME = "odin_test";
 module.exports = async () => {
   await prepare();
 
-  const server = new Server("mongod", {
+  const server = new Server(path.resolve(mongod_path, "bin", "mongod"), {
     dbpath,
     port: await portfinder.getPortPromise(),
     storageEngine: "wiredTiger",
