669b5772fcc928513cc1ad5c967efe0ada61c85f
Ardalan Amini <ardalanamini22@gmail.com>
2019-02-07 15:39:08 +0330

    WIP

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,8 @@
 const mongodb = require("mongodb");
 const Server = require("mongodb-memory-server");
 
+// const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 const DB_NAME = "odin_test";
 
 const MONGOD = new Server.default({
@@ -14,7 +16,26 @@ const MONGOD = new Server.default({
   },
 });
 
+// const REPLICA = new Server.MongoMemoryReplSet({
+//   autoStart: false,
+//   binary: {
+//     version: "4.0.5",
+//   },
+//   instanceOpts: [
+//     {
+//       storageEngine: "wiredTiger",
+//     },
+//   ],
+//   replSet: {
+//     name: "rs",
+//     dbName: DB_NAME,
+//     storageEngine: "wiredTiger",
+//   },
+// });
+
 module.exports = async () => {
+  global.__DB_NAME__ = DB_NAME;
+
   await MONGOD.start();
 
   global.__CONNECTION__ = await mongodb.connect(
@@ -23,6 +44,24 @@ module.exports = async () => {
     }
   );
 
-  global.__DB_NAME__ = DB_NAME;
   global.__MONGOD__ = MONGOD;
+
+  // await REPLICA.start();
+
+  // await REPLICA.waitUntilRunning();
+
+  // await sleep(2000);
+
+  // global.__REPLICA_CONN__ = await mongodb.connect(
+  //   await REPLICA.getConnectionString(), {
+  //     useNewUrlParser: true,
+  //     readConcern: {
+  //       level: "majority",
+  //     },
+  //     // connectWithNoPrimary: true,
+  //     replicaSet: "rs"
+  //   }
+  // );
+
+  // global.__REPLICA__ = REPLICA;
 }
